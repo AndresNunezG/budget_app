8023ee43aac0f6d9c56d993d4bd2c128d79a3821
AndresN <camilonunezz9812@gmail.com>
2021-05-27 22:40:00 -0500

    modulo operaciones para la logica de las transacciones

---
+++ operations.py
@@ -0,0 +1,19 @@
+from budget import categoria
+
+def realizar_operacion(operacion, cantidad, descripcion, cat_a, cat_b, dict_categorias):
+    if cat_a not in dict_categorias.keys():
+        dict_categorias[cat_a] = categoria(cat_a)
+    if cat_b not in dict_categorias.keys():
+        dict_categorias[cat_b] = categoria(cat_b)
+
+    cat_a_obj = dict_categorias[cat_a]
+    cat_b_obj = dict_categorias[cat_b]
+
+    if operacion == 1:
+        cat_a_obj.deposito(cantidad, descripcion)
+    elif operacion == 2:
+        cat_a_obj.retiro(cantidad, descripcion)
+    elif operacion == 3:
+        cat_a_obj.transferir(cantidad, cat_b)
+    print(cat_a_obj.registro)
+    return True
